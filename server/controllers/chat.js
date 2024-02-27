48f161daf1589bbaca1e10d2f54fae8dacb6dfc1
Thirukandiyur Sudarshan Karthk <thirusudarshan@gmail.com>
2024-02-27 19:53:54 +0530

    fix error hanling

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,48 +1,56 @@
-import { MessageModel } from "../model/chatModel.js"
-import { RoomModel} from "../model/roomModel.js"
-import { UserModel } from "../model/userModel.js"
+import { MessageModel } from "../model/chatModel.js";
+import { RoomModel } from "../model/roomModel.js";
+import { UserModel } from "../model/userModel.js";
 
-export const getRoom = async (req,res) => {
-    
-    const { friend } = req.query
+export const getRoom = async (req, res) => {
+  try {
+    const { friend } = req.query;
 
-    const frienddata = await UserModel.findOne({userName: friend})
+    const frienddata = await UserModel.findOne({ userName: friend });
     const user = await UserModel.findById(req.verifiedUser);
-    if (!frienddata) return res.status(401).json({"msg": "Friend not found"})
+    if (!frienddata) return res.status(401).json({ msg: "Friend not found" });
 
-    if (!user.friends.includes(friend)) return res.status(401).json({"msg": "You are not friends with user"}) 
+    if (!user.friends.includes(friend))
+      return res.status(401).json({ msg: "You are not friends with user" });
 
-    const room = await RoomModel.findOne({users: [user.id,friend.id]})
+    const room = await RoomModel.findOne({ users: [user.id, friend.id] });
 
-    if (room) return res.status(200).json({id: room.id}) 
+    if (room) return res.status(200).json({ id: room.id });
 
     const newroom = new RoomModel({
-        users: [user.id,frienddata.id]
-    })
+      users: [user.id, frienddata.id],
+    });
 
-    const savedroom = await newroom.save()
+    const savedroom = await newroom.save();
 
-    return res.status(200).json({id: savedroom.id})
-}
+    return res.status(200).json({ id: savedroom.id });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-export const sendMessage = async (req,res) => {
+export const sendMessage = async (req, res) => {
+  try {
+    const { roomId, message } = req.body;
+    const user = await UserModel.findById(req.verifiedUser);
 
-    const { roomId,message } = req.body
-    const user = await UserModel.findById(req.verifiedUser)
+    const room = await RoomModel.findById(roomId);
 
-    const room = await RoomModel.findById(roomId)
+    if (!room) return res.status(401).json({ msg: "Room dosn't exist" });
 
-    if (!room) return res.status(401).json({"msg": "Room dosn't exist"})
-
-    if (!room.users.includes(user.id)) return res.status(401).json({"msg": "User is not part of the room"})
+    if (!room.users.includes(user.id))
+      return res.status(401).json({ msg: "User is not part of the room" });
 
     const newMessage = new MessageModel({
-        body: message,
-        room: roomId,
-        sender: user.userName
-    })
+      body: message,
+      room: roomId,
+      sender: user.userName,
+    });
 
-    const savedmessage = await newMessage.save()
+    const savedmessage = await newMessage.save();
 
-    return res.status(200).json(savedmessage)
-}
+    return res.status(200).json(savedmessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
